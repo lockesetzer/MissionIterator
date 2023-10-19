715689081cea62458be4b9e396d10149baae8bf1
Kevin Caulfield <kevin.c.caulfield@gmail.com>
2023-10-19 18:17:13 -0400

    added logic for processing patching; patching dataset included

--- Program.cs
+++ Program.cs
@@ -8,10 +8,19 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using Microsoft.VisualBasic.FileIO;
 using System.Linq;
+using System.Collections.Generic;
 
 
 namespace MissionIterator
 {
+
+    class Patch
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+        public int Slot { get; set; }
+    }
+
     class Program
     {
         //static private String fileMission = "C:\\Users\\kevin\\Documents\\Projects\\EH\\XWTC3\\Waistem.XWI"; // XW
@@ -20,15 +29,58 @@ namespace MissionIterator
         //static private String fileMission = "C:\\Users\\kevin\\Documents\\Projects\\EH\\BoPTC1\\CRUCOMP1.TIE"; // BoP
         //static private String fileMission = "C:\\Users\\kevin\\Documents\\Projects\\EH\\XWATC67\\1B8M1Karana.tie"; // XWA
 
-        static private String dataStream = "C:\\Users\\kevin\\Documents\\Projects\\EH\\files.csv";
-        static private String shipFile = "C:\\Users\\kevin\\Documents\\Projects\\EH\\ships.csv";
-        class MissionList
-        {
-            public string Path { get; set; }
-        }
+        static private String dataStream = "..\\..\\..\\files.csv";
+        static private String patchStream = "..\\..\\..\\patch.csv";
+        static private String shipFile = "..\\..\\..\\ships.csv";
+
+        static private int patchIDField = 0;
+        static private int patchNameField = 1;
+        static private int patchSlotField = 2;
+
+        static private int missionPathField = 20;
+        static private int missionPatchIdsField = 15;
 
         static void Main(string[] args)
         {
+
+            Dictionary<int, Patch> patches = new Dictionary<int, Patch>();
+
+            FileStream ps = File.OpenRead(patchStream);
+
+            using TextFieldParser patchParser = new TextFieldParser(ps)
+            {
+                Delimiters = new[] { "," },
+                HasFieldsEnclosedInQuotes = true,
+                TextFieldType = FieldType.Delimited,
+                TrimWhiteSpace = true,
+            };
+
+            while (!patchParser.EndOfData)
+            {
+                var fields = patchParser.ReadFields() ?? throw new InvalidOperationException("Parser unexpectedly returned no data");
+
+                try
+                {
+                    int patchID = Int32.Parse(fields[patchIDField]);
+                    string patchName = fields[patchNameField];
+                    int patchSlot = Int32.Parse(fields[patchSlotField]);
+
+                    Patch patch = new Patch();
+                    patch.ID = patchID;
+                    patch.Name = patchName;
+                    patch.Slot = patchSlot;
+
+                    patches[patchID] = patch;
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine("Skipping patch ID " + fields[0]);
+                    Console.WriteLine(e);
+                }
+
+
+            }
 
             Boolean first = true;
 
@@ -70,7 +122,8 @@ namespace MissionIterator
 
                 else
                 {
-                    String fileMission = fields[fields.Length - 1];
+                    String filePatches = fields[missionPatchIdsField];
+                    String fileMission = fields[missionPathField];
 
                     Console.WriteLine(fileMission);
 
@@ -90,6 +143,30 @@ namespace MissionIterator
                         {
                             mission.LoadFromStream(fs);
 
+                            string[] craftType = Idmr.Platform.Xwing.Strings.CraftType;
+
+                            if (filePatches != null && filePatches.Length > 0)
+                            {
+                                string[] missionPatches = filePatches.Split('|');
+
+                                foreach (var missionPatch in missionPatches)
+                                {
+                                    try
+                                    {
+                                        int patchID = Int32.Parse(missionPatch);
+                                        Patch patch = patches[patchID];
+                                        Console.WriteLine("Applying " + patch.Name + "," + patch.Slot);
+                                        craftType[patch.Slot] = patch.Name;
+        
+                            }
+                                    catch (Exception e)
+                                    {
+                                        Console.WriteLine("Could not parse " + missionPatch);
+                                        Console.WriteLine(e);
+                                    }
+                                }
+                            }
+
                             Idmr.Platform.Xwing.FlightGroupCollection fgs = mission.FlightGroups;
 
                             foreach (Idmr.Platform.Xwing.FlightGroup fg in fgs)
@@ -108,7 +185,6 @@ namespace MissionIterator
                                     Console.WriteLine(fg.ToString());
 
                                     byte shipType = fg.CraftType;
-                                    string[] craftType = Idmr.Platform.Xwing.Strings.CraftType;
                                     //Console.WriteLine(craftType[shipType]);
                                     playerShip = craftType[shipType];
                                     shipName = fg.Name;
@@ -142,6 +218,30 @@ namespace MissionIterator
                         {
                             mission.LoadFromStream(fs);
 
+                            string[] craftType = Idmr.Platform.Tie.Strings.CraftType;
+
+                            if (filePatches != null && filePatches.Length > 0)
+                            {
+                                string[] missionPatches = filePatches.Split('|');
+
+                                foreach (var missionPatch in missionPatches)
+                                {
+                                    try
+                                    {
+                                        int patchID = Int32.Parse(missionPatch);
+                                        Patch patch = patches[patchID];
+                                        Console.WriteLine("Applying " + patch.Name + "," + patch.Slot);
+                                        craftType[patch.Slot] = patch.Name;
+        
+                            }
+                                    catch (Exception e)
+                                    {
+                                        Console.WriteLine("Could not parse " + missionPatch);
+                                        Console.WriteLine(e);
+                                    }
+                                }
+                            }
+
                             Idmr.Platform.Tie.FlightGroupCollection fgs = mission.FlightGroups;
 
                             foreach (Idmr.Platform.Tie.FlightGroup fg in fgs)
@@ -160,7 +260,6 @@ namespace MissionIterator
                                     Console.WriteLine(fg.ToString());
 
                                     byte shipType = fg.CraftType;
-                                    string[] craftType = Idmr.Platform.Tie.Strings.CraftType;
                                     //Console.WriteLine(craftType[shipType]);
                                     //Console.WriteLine(fg.Name);
 
@@ -195,6 +294,30 @@ namespace MissionIterator
                         {
                             mission.LoadFromStream(fs);
 
+                            string[] craftType = Idmr.Platform.Xvt.Strings.CraftType;
+
+                            if (filePatches != null && filePatches.Length > 0)
+                            {
+                                string[] missionPatches = filePatches.Split('|');
+
+                                foreach (var missionPatch in missionPatches)
+                                {
+                                    try
+                                    {
+                                        int patchID = Int32.Parse(missionPatch);
+                                        Patch patch = patches[patchID];
+                                        Console.WriteLine("Applying " + patch.Name + "," + patch.Slot);
+                                        craftType[patch.Slot] = patch.Name;
+        
+                            }
+                                    catch (Exception e)
+                                    {
+                                        Console.WriteLine("Could not parse " + missionPatch);
+                                        Console.WriteLine(e);
+                                    }
+                                }
+                            }
+
                             Idmr.Platform.Xvt.FlightGroupCollection fgs = mission.FlightGroups;
 
                             foreach (Idmr.Platform.Xvt.FlightGroup fg in fgs)
@@ -213,11 +336,10 @@ namespace MissionIterator
                                     Console.WriteLine(fg.ToString());
 
                                     byte shipType = fg.CraftType;
-                                    string[] craftType = Idmr.Platform.Xvt.Strings.CraftType;
                                     //Console.WriteLine(craftType[shipType]);
                                     //Console.WriteLine(fg.Name);
 
-                                    playerShip = craftType[shipType];
+                                    playerShip = craftType[shipType].Replace("*", "");
                                     shipName = fg.Name;
                                     String shipLine = line + "," + playerShip + "," + shipName;
                                     ships.AppendLine(shipLine);
@@ -248,8 +370,32 @@ namespace MissionIterator
                         {
                             mission.LoadFromStream(fs);
 
-                            Idmr.Platform.Xvt.FlightGroupCollection fgs = mission.FlightGroups;
+                            string[] craftType = Idmr.Platform.Xvt.Strings.CraftType;
 
+                            if (filePatches != null && filePatches.Length > 0)
+                            {
+                                string[] missionPatches = filePatches.Split('|');
+
+                                foreach (var missionPatch in missionPatches)
+                                {
+                                    try
+                                    {
+                                        int patchID = Int32.Parse(missionPatch);
+                                        Patch patch = patches[patchID];
+                                        Console.WriteLine("Applying " + patch.Name + "," + patch.Slot);
+                                        craftType[patch.Slot] = patch.Name;
+        
+                            }
+                                    catch (Exception e)
+                                    {
+                                        Console.WriteLine("Could not parse " + missionPatch);
+                                        Console.WriteLine(e);
+                                    }
+                                }
+                            }
+
+                            Idmr.Platform.Xvt.FlightGroupCollection fgs = mission.FlightGroups;
+                            
                             foreach (Idmr.Platform.Xvt.FlightGroup fg in fgs)
                             {
 
@@ -266,10 +412,9 @@ namespace MissionIterator
                                     Console.WriteLine(fg.ToString());
 
                                     byte shipType = fg.CraftType;
-                                    string[] craftType = Idmr.Platform.Xvt.Strings.CraftType;
                                     //Console.WriteLine(craftType[shipType]);
                                     //Console.WriteLine(fg.Name);
-                                    playerShip = craftType[shipType];
+                                    playerShip = craftType[shipType].Replace("*", "");
                                     shipName = fg.Name;
                                     String shipLine = line + "," + playerShip + "," + shipName;
                                     ships.AppendLine(shipLine);
@@ -300,6 +445,30 @@ namespace MissionIterator
                         {
                             mission.LoadFromStream(fs);
 
+                            string[] craftType = Idmr.Platform.Xwa.Strings.CraftType;
+
+                            if (filePatches != null && filePatches.Length > 0)
+                            {
+                                string[] missionPatches = filePatches.Split('|');
+
+                                foreach (var missionPatch in missionPatches)
+                                {
+                                    try
+                                    {
+                                        int patchID = Int32.Parse(missionPatch);
+                                        Patch patch = patches[patchID];
+                                        Console.WriteLine("Applying " + patch.Name + "," + patch.Slot);
+                                        craftType[patch.Slot] = patch.Name;
+        
+                            }
+                                    catch (Exception e)
+                                    {
+                                        Console.WriteLine("Could not parse " + missionPatch);
+                                        Console.WriteLine(e);
+                                    }
+                                }
+                            }
+
                             Idmr.Platform.Xwa.FlightGroupCollection fgs = mission.FlightGroups;
 
                             foreach (Idmr.Platform.Xwa.FlightGroup fg in fgs)
@@ -318,7 +487,6 @@ namespace MissionIterator
                                     Console.WriteLine(fg.ToString());
 
                                     byte shipType = fg.CraftType;
-                                    string[] craftType = Idmr.Platform.Xwa.Strings.CraftType;
                                     //Console.WriteLine(craftType[shipType]);
                                     //Console.WriteLine(fg.Name);
 
